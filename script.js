6b30c7f22c4f66346ee7c2666b9153c85bde1032
Prof. Thiago Vieira de Aguiar <>
2022-08-05 12:08:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+function apertar(){
+  alert("Botão clickado.");
+}
+
+// Acessar o botão
+// Elemento da DOM
+btnClique.addEventListener('click', apertar);
+
+
+// numeroA = 0;
+// numeroB = 0;
+
+// numeroA = prompt("Informe um número:"); // 10 10.6
+// numeroB = prompt("Informe outro número:"); // 2
+
+// Converter
+// numeroA = Number(numeroA);
+// numeroB = Number(numeroB);
+
+// console.log(numeroA + numeroB);
+
+// nome = 0;
+// nome = prompt("Qual é o seu nome?");
+// console.log("Olá, " + nome);
+// alert("Olá, " + nome);
+
+// console.log("3" + 1); // '31'
+// console.log("3" + "1")
+// console.log("3" - 2); // 1
+// console.log("3" * 2); // 1
+// console.log("3" / 2); // 1
+
+// console.log("dez" / 2); // 1 // NaN
+// console.log(3 - 2);
+
+
+// variável
+// primeiroNumero = 10;
+// segundoNumero = 14;
+// primeiroNumero = "JavaScript";
+// soma = primeiroNumero + segundoNumero;
+// console.log(soma);
+// alert(nomeVariavel);
+
+/*
+linha 1
+linha 2
+linha 3
+...
+*/
