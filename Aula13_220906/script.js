787c13439b080f687c61fc74750356da693f6e03
Prof. Thiago Vieira de Aguiar <56936784+thiago-aguiar@users.noreply.github.com>
2022-09-08 11:16:28 +0000

    Aula 13: 06/09/22: Funções: Introdução.

---
+++ Aula13_220906/script.js
@@ -0,0 +1,108 @@
+// Aula 13
+
+function verificarParOuImpar(num){
+  // let resposta = "";
+  if (num > 0){ 
+    return  `${num} eh par.`; 
+  } else if (num < 0) { 
+    return  `${num} eh ímpar`; 
+  } else {
+    console.log("Não identificado.");
+  }
+  // return resposta;
+}
+
+// verificarParOuImpar(2);
+alert(verificarParOuImpar(2));
+/* const ehPar = verificarParOuImpar(2);
+console.log(ehPar); */
+
+// function numeDaFuncao(){
+//   // código da funcao
+//   return "Retorno";
+// }
+
+// const retorno = numeDaFuncao();
+
+
+// const retorno = prompt("Informe algo!");
+// console.log(prompt("Informe algo!"));
+// prompt("Informe algo!");
+
+/* function operar(numA, numB, operador){
+  switch(operador){
+    case "+": console.log("Soma: ", numA + numB); break;
+    case "-": console.log("Sub: ", numA - numB); break;
+    case "*": console.log("Mult: ", numA * numB); break;
+    case "/": console.log("Div: ", numA / numB); break;
+    default: console.log("Div: ", numA / numB);
+  }
+}
+
+operar(5, 10, "+", 3, 4, 5, 6, 7);
+operar(5, 10, "-", "ignorados");
+operar(5, 10, "*");
+operar(5, 10, "/"); */
+
+/* function nomeDaFuncao(arg1, arg2, arg3){
+  // argumento = "Argumento2"
+  console.log(arg1, arg2, arg3);
+}
+
+nomeDaFuncao("A3",    "A1",    "A2"); */
+
+/* function quadrado(num) {
+  const quadrado = num ** 2;
+  console.log(`O quadrado de ${num} eh ${quadrado}`);
+}
+
+quadrado(2);
+quadrado(10);
+quadrado(965874); */
+
+/* function verificarParOuImpar(num){
+  if (num % 2 == 0){ console.log(num, "eh par."); }
+  else { console.log(num, "eh ímpar"); }
+}
+
+const num = prompt("Informe um número:");
+verificarParOuImpar(num); */
+
+// verificarParOuImpar(2);
+// verificarParOuImpar(67);
+// verificarParOuImpar(99);
+// verificarParOuImpar(54);
+// verificarParOuImpar();
+
+// function nomeDaFuncao(argumento){
+//   // argumento = "Argumento2"
+//   console.log(argumento);
+// }
+
+// const nome = "Nome A"
+// nomeDaFuncao(nome);
+// nomeDaFuncao("Argumento2");
+
+/* let opcao;
+let menu = "Escolha uma opção:\n";
+  menu += "1 - Inserir Usuário.\n";
+  menu += "2 - Listar Usuário.\n";
+  menu += "Opção: ";
+
+function imprimirMenu(){
+  opcao = prompt(menu);
+}
+
+imprimirMenu();
+console.log(opcao); */
+
+// function nomeDaFuncao() {
+//   // alert("Função: nomeDaFuncao");
+//   console.log("Função...");
+// }
+
+// nomeDaFuncao();
+// nomeDaFuncao();
+// console.log("Fim do programa.");
+
+
